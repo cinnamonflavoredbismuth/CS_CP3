29e70886c22f48997447b99dac9b9f91d9c08947
cinnamonflavoredbismuth <cecily.strong@ucas-edu.net>
2025-08-25 11:47:38 -0600

    notes

--- Notes/Strings, Arrays, conditionals/main.cpp
+++ Notes/Strings, Arrays, conditionals/main.cpp
@@ -1,20 +1,34 @@
 // CS Strings, Arrays, conditionals
-    
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
 // What is Narrowing?
-    //
+    // you take a variable that already exists and moves its contents into a variable that takes up a smaller amount of space. you will loose information bc theres not enough space
+
+// for big numnbers, use apostrophies instead of commas
+
 // What is a basic way to generate random numbers in C++?
-    //
+    // seed the random
 // What is an array?
-    //
+    // 
 // How do I create an array?
-    //
+    // 
 // How do you make strings in C?
-    //
+    // 
 // How did C++ improve creating strings? 
-    //
+    // 
 // How do I search a string?
-    //
+    // 
 // How do I modify a string?
-    //
+    // 
 // What are some string methods? 
-    //
+    // 
+
+int main(){
+    int sec = time(nullptr);
+    srand(sec) ;
+    int my_num = rand() % 11;
+    cout << my_num;
+    return 0; 
+}
--- user-login/main.cpp
+++ user-login/main.cpp
@@ -1,8 +1,49 @@
 // CS User Login
 // Write a program that takes in a user name and checks to see if that user is an admin, returning user, or a new user and then says hello to them accordingly. 
+/*EXAMPLE:
+Hello Admin Ms.LaRose
+Welcome back Andrew
+Lets get you signed up Katie*/
+
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
+    string admins[2] = {"cecily","mrs.larose"};
+    string returning_users[2]= {"andrew","john"};
+    string username = "";
+    cout << "Enter your username: ";
+    cin >> username;
+    int num=0;
+    bool found = false;
+    while (found == false && num < 2) {
+        if (username == admins[num]) {
+            cout << "Hello Admin " << username;
+            found = true;
+        }
+        else if (username == returning_users[num]) {
+            cout << "Welcome back " << username;
+            found = true;
+        }
+        else if (num == 1) {
+            string new_user;
+            cout << "Lets get you signed up " << username;
+            cout << "\nPlease enter a new username: ";
+            cin >> new_user;
+            cout << "You are now signed up, " << new_user;
+
+
+            found = true;
+        }
+        num++;
+    }
+    
+    /*cin >> username;
+    if username in admins {
+        cout << "Hello Admin " << username;
+    } else {
+        cout << "Welcome back " << username;
+    };"*/
     return 0;
 }
