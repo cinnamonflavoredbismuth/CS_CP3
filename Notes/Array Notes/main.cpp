ae84a54e7f551115a4290b55fd45d274468db3b7
cinnamonflavoredbismuth <cecily.strong@ucas-edu.net>
2025-08-29 11:38:41 -0600

    finished notes

--- Notes/Array Notes/main.cpp
+++ Notes/Array Notes/main.cpp
@@ -16,6 +16,8 @@
 // How do you search an array
     // using for loops and checking each item
 // How do you make a multi-dimensional array
+    // var[row][column]{{1,2,3}, {1,2,3}, {1,2,3}}
+// how to sort an array
     // 
 #include <iostream>
 #include <limits>
@@ -36,32 +38,59 @@ int search(string list[], size_t len, string item){
 }
     
 
+int sort(int list[],int size){
+    for (int j = 0; j < size; j++){
+        for (int i = 1; i < (size); i++){
+            if (list[i] < list[i-1]){ // THIS IS WHERE YOU WANT IT IN ORDER OR REVERSE ORDER
+                int a = list[i];
+                list[i] = list[i-1];
+                list[i-1] = a;
+            }
+        }
+    }
+    return 0;
+}
+
+int prnt_list(int list[],int size){
+    for(int i = 0; i < size; i++){
+         cout << list[i] << " ";}
+    cout << "\n";
+    return 0;
+}
+
 int main(){
     // cout << fam << endl;
     //search(fam, "Cecily");
-    for(int i = 0; i < size(fam); i++){
-        cout << fam[i] << " Strong\n";
-        search(sibs, size(sibs), fam[i]);
-        // 
-        //     cout << "They are a sibling!\n";
-    }
+    // for(int i = 0; i < size(fam); i++){
+    //     cout << fam[i] << " Strong\n";
+    //     search(sibs, size(sibs), fam[i]);
+    //     // 
+    //     //     cout << "They are a sibling!\n";
+    // }
 
-    // SIZE OF T
-    cout << numeric_limits <long long>::min()<<endl;
-    cout << numeric_limits <long long>::max()<<endl;
-    cout << numeric_limits <size_t>::min()<<endl;
-    cout << numeric_limits <size_t>::max()<<endl;
+    // // SIZE OF T
+    // cout << numeric_limits <long long>::min()<<endl;
+    // cout << numeric_limits <long long>::max()<<endl;
+    // cout << numeric_limits <size_t>::min()<<endl;
+    // cout << numeric_limits <size_t>::max()<<endl;
 
-    //STRUCTURED BINDING
-    auto [q,w,e,r,t] = fam;
-    cout << e << endl;
-    
+    // //STRUCTURED BINDING
+    // auto [q,w,e,r,t] = fam;
+    // cout << e << endl;
+
+    // // MULTI DIMENSIONAL LIST
+    // int matrix[3][3] = {{1,2,3},
+    //                     {4,5,6},
+    //                     {7,8,9}};   
+    int list[6]={3,1,5,4,7,9};
+    prnt_list(list,size(list));
+
+    sort(list,size(list));
+
+    prnt_list(list,size(list));
 
     return 0;
 
 
-    // MULTI DIMENSIONAL LIST
-    int matrix = {{1,2,3},
-                  {1,2,3},
-                  {1,2,3}};
+
 }
--- tic-tac-toe/main.cpp
+++ tic-tac-toe/main.cpp
@@ -8,6 +8,22 @@ using namespace std;
    |   |   |
    |   |   |
    |   |   |*/
+string board[3][3] = { 
+    {" "," "," "}, 
+    {" "," "," "}, 
+    {" "," "," "}
+ };
+
+auto [a,b,c] = board;
+
+int check (string board){
+    for (int i = 0; i < 3; i++){
+        if (a[i]==b[i] and b[i]==c[i]){
+            return 1;
+        };
+    };
+    return 0;
+}
 
 int random_number(int max){
     int sec = time(nullptr);
@@ -17,7 +33,20 @@ int random_number(int max){
     return my_num;
 }
 
+int prnt_board(auto board){
+    for(int row = 0; row < 3; row++){
+        string item[3] = board[row]
+        for (int i = 0; i < 3; i++){
+            cout << item[i] << " ";
+        }
+        cout << '\n';
+    }
+    return 0 
+}
+
+
 int main(){
+    prnt_board(board);
     cout << "" ;
     return 0;
 }
