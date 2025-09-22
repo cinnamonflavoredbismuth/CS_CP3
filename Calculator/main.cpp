05c747d2808f4ec0501a0eec67b5525efe6084f8
cinnamonflavoredbismuth <cecily.strong@ucas-edu.net>
2025-09-22 11:04:21 -0600

    enumeration

---
+++ Calculator/main.cpp
@@ -0,0 +1,7 @@
+//CS Calculator
+/*Write a program that creates a user menu for a basic calculator using enumeration in C++. The calculator should be able to perform addition, subtraction, multiplication, and division. The program should display a menu of options, allow the user to select an operation, input two numbers, perform the calculation, and display the result. The program must continue to run till the user chooses to exit and have proper input handling. */
+
+int main(){
+    
+    return 0;
+}
---
+++ Notes/enumeration/main.cpp
@@ -0,0 +1,79 @@
+/* CS Enumeration Notes
+
+What is enumeration?
+A more convinent way to work with sets of input options.
+
+How do you build enumeration?
+enum Action{
+    List = 1,
+    Add = 2,
+    Update = 3
+};
+
+How do we display our enumerator? 
+cout << "1: List invoices" << endl <<
+    "2: Add invoices" << endl <<
+    "3: Update invoices" << endl <<
+    "Select: ";
+
+Why does it matter that enumerators are constants?
+So that we don't accidentally change the values.
+
+What is the default beginning enumerator? 
+0,1,2,3,etc
+
+How do you give a different default value?
+enum Action{
+    List = 1,
+    Add = 2,
+    Update = 3
+};
+
+What is a strongly typed enumerator?
+enum class Emu{
+    List = 1,
+    Add = 2,
+    Update = 3
+};
+
+How are strongly typed enumerators different from a normal one?
+We have to specifically cast them to compare stuff.
+
+
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+enum Action{
+    List = 1,
+    Add = 2,
+    Update = 3
+};
+
+enum class Emu{
+    List = 1,
+    Add = 2,
+    Update = 3
+};
+
+int main() {
+    int input;
+
+    cout << "1: List invoices" << endl <<
+    "2: Add invoices" << endl <<
+    "3: Update invoices" << endl <<
+    "Select: ";
+    cin >> input;
+    if (input == Action::List) {
+        cout << "List invoices" << endl;
+    }else if (input == Action::Add) {
+        cout << "Add invoices" << endl;
+    }else if (input == static_cast<int>(Emu::Update)) {
+        cout << "Deciever, deciever, slice thine pantaloons with a cleaver!" << endl;
+    }else{
+        cout << "Womp womp." << endl;
+    }
+    return 0;
+}
