d8e1d24c2779c9875df58214a865e4600510bc67
cinnamonflavoredbismuth <cecily.strong@ucas-edu.net>
2025-09-23 11:47:56 -0600

    tried type setup

--- copyrightmon/main.cpp
+++ copyrightmon/main.cpp
@@ -15,14 +15,25 @@ The game must include the following features:
 #include <vector>
 using namespace std;
 
-enum class Types{
-    Fire = 1,
-    Water = 2,
-    Grass = 3,
-    Ground = 4,
-    Electric = 5
+int input(){ 
+    int input=0;   
+    while(!(cin >> input)){
+        cout<<"Please enter a number option: "<<endl;
+        cin.clear();
+        cin.ignore(10000);
+    }
+    return input;
+    }
+
+enum class Type{
+    Fire = 0,
+    Water = 1,
+    Grass = 2,
+    Ground = 3,
+    Electric = 4
 };
-enum class Fire_Attacks{
+
+enum class Fire_attacks{
     Gender_reveal_party = 1,
     The_sun = 2,
     Firenado = 3,
@@ -32,10 +43,55 @@ enum class Water_attacks{
     Erosion = 2,
     Evaporation = 3
 };
-struct copyrightmon {
-    int type;
-    string name;
-    int weakness;
-    int strength;
-    int attacks[];
+enum class Grass_attacks{
+    Allergic_reaction = 1,
+    Pokey_grass = 2,
+    Leaf = 3
 };
+enum class Ground_attacks{
+    Trip_over_a_rock = 1,
+    Falling_debris = 2,
+    Dust_in_your_eyes = 3
+};
+enum class Electric_attacks{
+    Exposed_wire = 1,
+    Static_electricity = 2,
+    Bzzt = 3
+};
+
+
+struct attack_options {
+    string attack1;
+    string attack2;
+    string attack3;
+};
+
+struct Type_info{
+    string name;
+    int num;
+    Types strengths;
+    Types weaknesses;
+    attack_options attacks;
+};
+struct Types{
+    Type_info fire_type;
+    Type_info water_type;
+    Type_info grass_type;
+    Type_info ground_type;
+    Type_info electric_type;
+    
+};
+
+int type_setup(){
+    Types types;
+    types.fire_type.name = "fire";
+    types.fire_type.num = 0;
+    types.fire_type.strengths=types.grass_type;
+    
+return 0;
+};
+
+int main(){
+    type_setup();
+    return 0;
+}
