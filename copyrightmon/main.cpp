0c5e26e50bb2d65e2e22ff1e51e857a7d8c0f5af
cinnamonflavoredbismuth <cecily.strong@ucas-edu.net>
2025-09-24 11:48:37 -0600

    battle function started. still working on fixing types

--- copyrightmon/main.cpp
+++ copyrightmon/main.cpp
@@ -25,13 +25,7 @@ int input(){
     return input;
     }
 
-enum class Type{
-    Fire = 0,
-    Water = 1,
-    Grass = 2,
-    Ground = 3,
-    Electric = 4
-};
+
 
 enum class Fire_attacks{
     Gender_reveal_party = 1,
@@ -60,36 +54,119 @@ enum class Electric_attacks{
 };
 
 
-struct attack_options {
-    string attack1;
-    string attack2;
-    string attack3;
-};
-
-struct Type_info{
+struct Type{
     string name;
     int num;
-    Types strengths;
-    Types weaknesses;
-    attack_options attacks;
+    int strengths;
+    int weaknesses;
+
 };
+
 struct Types{
-    Type_info fire_type;
-    Type_info water_type;
-    Type_info grass_type;
-    Type_info ground_type;
-    Type_info electric_type;
+    Type fire_type;
+    Type water_type;
+    Type grass_type;
+    Type ground_type;
+    Type electric_type;
+    
+};
+Types types;
+
+int type_setup(){
+    //fire type
+    types.fire_type.name = "fire";
+    types.fire_type.num = 0;
+    types.fire_type.strengths=types.grass_type.num;
+    types.fire_type.weaknesses=types.water_type.num;
+    //water type
+    types.water_type.name = "water";
+    types.water_type.num = 1;
+    types.water_type.strengths=types.fire_type.num;
+    types.water_type.weaknesses=types.electric_type.num;
+    //grass type
+    types.grass_type.name = "grass";
+    types.grass_type.num = 2;   
+    types.grass_type.strengths=types.ground_type.num;
+    types.grass_type.weaknesses=types.fire_type.num;
+    //ground type
+    types.ground_type.name = "ground";
+    types.ground_type.num = 3;
+    types.ground_type.strengths=types.electric_type.num;
+    types.ground_type.weaknesses=types.grass_type.num;
+    //electric type
+    types.electric_type.name = "electric";
+    types.electric_type.num = 4;
+    types.electric_type.strengths=types.water_type.num;
+    types.electric_type.weaknesses=types.ground_type.num;
+
+    return 0;
     
 };
 
-int type_setup(){
-    Types types;
-    types.fire_type.name = "fire";
-    types.fire_type.num = 0;
-    types.fire_type.strengths=types.grass_type;
-    
-return 0;
+struct Attack{
+    string name;
+    int power;
+    Type type;
 };
+
+
+struct Copyrightmon{
+    string name;
+    int max_hp;
+    int current_hp;
+    Type type;
+    int level;
+    Attack attacks[3];
+};
+
+Copyrightmon pikachu{
+    "Pikachu",
+    100,
+    100,
+    types.electric_type,
+    5,
+    {{"Exposed wire", 15, types.electric_type},
+    {"Static electricity", 20, types.electric_type},
+    {"Bzzt", 25, types.electric_type}}
+};
+
+int calculate_damage(Copyrightmon& attacker, Copyrightmon& defender, int attack_choice){
+    int base_damage = 10; // Base damage for all attacks
+    // Type effectiveness
+    base_damage += attacker.level * 2; // Increase damage based on level
+    if(attacker.type.strengths == defender.type.num){
+        base_damage *= 2; // Double damage for strengths
+    } else if(attacker.type.weaknesses == defender.type.num){
+        base_damage /= 2; // Half damage for weaknesses
+    }
+    return base_damage;
+}
+
+int battle(Copyrightmon& player, Copyrightmon& opponent){
+    cout << "A wild " << opponent.name << " appeared!" << endl;
+    while(player.current_hp > 0 && opponent.current_hp > 0){
+        cout << "Choose an attack:" << endl;
+        cout << "1. " << player.attacks[0].name << endl;
+        cout << "2. "<< player.attacks[1].name << endl;
+        cout << "3. "<< player.attacks[2].name << endl;
+        int attack_choice = input();
+        int damage = calculate_damage(player, opponent, attack_choice);
+        opponent.current_hp -= damage;
+        cout << player.name << " used attack " << attack_choice << " and dealt " << damage << " damage!" << endl;
+        if(opponent.current_hp <= 0){
+            cout << opponent.name << " fainted!" << endl;
+            break;
+        }
+        int opp_damage = calculate_damage(opponent, player, 1);
+        player.current_hp -= opp_damage; // Opponent uses first attack
+        cout << opponent.name << " attacked and dealt " << opp_damage << " damage!" << endl;
+        if(player.current_hp <= 0){
+            cout << player.name << " fainted!" << endl;
+            break;
+        }
+    }
+    return 0;
+}
 
 int main(){
     type_setup();
