0bb2005de1763947b88bcfaead70c1503b2bdf83
cinnamonflavoredbismuth <cecily.strong@ucas-edu.net>
2025-09-26 11:29:19 -0600

    created useful functions + made battle and explore functions (please incorperate when home)

--- copyrightmon/main.cpp
+++ copyrightmon/main.cpp
@@ -1,19 +1,193 @@
-//CS Pokemon
-/*Write a program that creates a Pokémon battle game using structures for Pokémon and enumeration for the menu in C++. The game should allow users to explore (find Pokémon), battle, heal Pokémon, and exit the game. Implement structures for Pokémon that include names, max HP, attacks, and level. Use enumeration for the main menu options.
+// //CS Pokemon
+// /*Write a program that creates a Pokémon battle game using structures for Pokémon and enumeration for the menu in C++. The game should allow users to explore (find Pokémon), battle, heal Pokémon, and exit the game. Implement structures for Pokémon that include names, max HP, attacks, and level. Use enumeration for the main menu options.
 
-The game must include the following features:
+// The game must include the following features:
 
-1. A main menu with options to explore, battle, heal, and exit.
-2. Exploration feature to find and catch Pokémon.
-3. Battle system where users can fight against wild Pokémon or other trainers.
-4. Healing option to restore Pokémon HP.
-5. Multiple attack options for each Pokémon during battles.
-6. Different Pokémon types with associated weaknesses and strengths.
-7. Type-based damage bonuses during battles.*/
+// 1. A main menu with options to explore, battle, heal, and exit.
+// 2. Exploration feature to find and catch Pokémon.
+// 3. Battle system where users can fight against wild Pokémon or other trainers.
+// 4. Healing option to restore Pokémon HP.
+// 5. Multiple attack options for each Pokémon during battles.
+// 6. Different Pokémon types with associated weaknesses and strengths.
+// 7. Type-based damage bonuses during battles.*/
+// #include <iostream>
+// #include <string>
+// #include <vector>
+// using namespace std;
+
+// int input(){ 
+//     int input=0;   
+//     while(!(cin >> input)){
+//         cout<<"Please enter a number option: "<<endl;
+//         cin.clear();
+//         cin.ignore(10000);
+//     }
+//     return input;
+//     }
+
+
+
+// enum class Fire_attacks{
+//     Gender_reveal_party = 1,
+//     The_sun = 2,
+//     Firenado = 3,
+// };
+// enum class Water_attacks{
+//     Light_shower = 1,
+//     Erosion = 2,
+//     Evaporation = 3
+// };
+// enum class Grass_attacks{
+//     Allergic_reaction = 1,
+//     Pokey_grass = 2,
+//     Leaf = 3
+// };
+// enum class Ground_attacks{
+//     Trip_over_a_rock = 1,
+//     Falling_debris = 2,
+//     Dust_in_your_eyes = 3
+// };
+// enum class Electric_attacks{
+//     Exposed_wire = 1,
+//     Static_electricity = 2,
+//     Bzzt = 3
+// };
+
+
+// struct Type{
+//     string name;
+//     int num;
+//     int strengths;
+//     int weaknesses;
+
+// };
+
+// struct Types{
+//     Type fire_type;
+//     Type water_type;
+//     Type grass_type;
+//     Type ground_type;
+//     Type electric_type;
+    
+// };
+// Types types;
+
+// int type_setup(){
+//     //fire type
+//     types.fire_type.name = "fire";
+//     types.fire_type.num = 0;
+//     types.fire_type.strengths=types.grass_type.num;
+//     types.fire_type.weaknesses=types.water_type.num;
+//     //water type
+//     types.water_type.name = "water";
+//     types.water_type.num = 1;
+//     types.water_type.strengths=types.fire_type.num;
+//     types.water_type.weaknesses=types.electric_type.num;
+//     //grass type
+//     types.grass_type.name = "grass";
+//     types.grass_type.num = 2;   
+//     types.grass_type.strengths=types.ground_type.num;
+//     types.grass_type.weaknesses=types.fire_type.num;
+//     //ground type
+//     types.ground_type.name = "ground";
+//     types.ground_type.num = 3;
+//     types.ground_type.strengths=types.electric_type.num;
+//     types.ground_type.weaknesses=types.grass_type.num;
+//     //electric type
+//     types.electric_type.name = "electric";
+//     types.electric_type.num = 4;
+//     types.electric_type.strengths=types.water_type.num;
+//     types.electric_type.weaknesses=types.ground_type.num;
+
+//     return 0;
+    
+// };
+
+// struct Attack{
+//     string name;
+//     int power;
+//     Type type;
+// };
+
+
+// struct Copyrightmon{
+//     string name;
+//     int max_hp;
+//     int current_hp;
+//     Type type;
+//     int level;
+//     Attack attacks[3];
+// };
+
+// Copyrightmon pikachu{
+//     "Pikachu",
+//     100,
+//     100,
+//     types.electric_type,
+//     5,
+//     {{"Exposed wire", 15, types.electric_type},
+//     {"Static electricity", 20, types.electric_type},
+//     {"Bzzt", 25, types.electric_type}}
+// };
+
+// int calculate_damage(Copyrightmon& attacker, Copyrightmon& defender, int attack_choice){
+//     int base_damage = 10; // Base damage for all attacks
+//     // Type effectiveness
+//     base_damage += attacker.level * 2; // Increase damage based on level
+//     if(attacker.type.strengths == defender.type.num){
+//         base_damage *= 2; // Double damage for strengths
+//     } else if(attacker.type.weaknesses == defender.type.num){
+//         base_damage /= 2; // Half damage for weaknesses
+//     }
+//     return base_damage;
+// }
+
+// int battle(Copyrightmon& player, Copyrightmon& opponent){
+//     cout << "A wild " << opponent.name << " appeared!" << endl;
+//     while(player.current_hp > 0 && opponent.current_hp > 0){
+//         cout << "Choose an attack:" << endl;
+//         cout << "1. " << player.attacks[0].name << endl;
+//         cout << "2. "<< player.attacks[1].name << endl;
+//         cout << "3. "<< player.attacks[2].name << endl;
+//         int attack_choice = input();
+//         int damage = calculate_damage(player, opponent, attack_choice);
+//         opponent.current_hp -= damage;
+//         cout << player.name << " used attack " << attack_choice << " and dealt " << damage << " damage!" << endl;
+//         if(opponent.current_hp <= 0){
+//             cout << opponent.name << " fainted!" << endl;
+//             break;
+//         }
+//         int opp_damage = calculate_damage(opponent, player, 1);
+//         player.current_hp -= opp_damage; // Opponent uses first attack
+//         cout << opponent.name << " attacked and dealt " << opp_damage << " damage!" << endl;
+//         if(player.current_hp <= 0){
+//             cout << player.name << " fainted!" << endl;
+//             break;
+//         }
+//     }
+//     return 0;
+// }
 #include <iostream>
 #include <string>
 #include <vector>
+
 using namespace std;
+
+
+// RANDOM FUNCTION
+
+#include <ctime>
+#include <chrono>
+#include <thread>
+
+int random_number(int max){
+    int sec = time(nullptr);
+    this_thread::sleep_for(chrono::seconds(1));
+    srand(sec) ;
+    int my_num = rand() % (max+1); //generates a number between 0 and the number you put in
+    //cout << my_num << endl;
+    return my_num;
+}
 
 int input(){ 
     int input=0;   
@@ -25,150 +199,63 @@ int input(){
     return input;
     }
 
-
-
-enum class Fire_attacks{
-    Gender_reveal_party = 1,
-    The_sun = 2,
-    Firenado = 3,
-};
-enum class Water_attacks{
-    Light_shower = 1,
-    Erosion = 2,
-    Evaporation = 3
-};
-enum class Grass_attacks{
-    Allergic_reaction = 1,
-    Pokey_grass = 2,
-    Leaf = 3
-};
-enum class Ground_attacks{
-    Trip_over_a_rock = 1,
-    Falling_debris = 2,
-    Dust_in_your_eyes = 3
-};
-enum class Electric_attacks{
-    Exposed_wire = 1,
-    Static_electricity = 2,
-    Bzzt = 3
-};
-
-
-struct Type{
-    string name;
-    int num;
-    int strengths;
-    int weaknesses;
-
-};
-
-struct Types{
-    Type fire_type;
-    Type water_type;
-    Type grass_type;
-    Type ground_type;
-    Type electric_type;
-    
-};
-Types types;
-
-int type_setup(){
-    //fire type
-    types.fire_type.name = "fire";
-    types.fire_type.num = 0;
-    types.fire_type.strengths=types.grass_type.num;
-    types.fire_type.weaknesses=types.water_type.num;
-    //water type
-    types.water_type.name = "water";
-    types.water_type.num = 1;
-    types.water_type.strengths=types.fire_type.num;
-    types.water_type.weaknesses=types.electric_type.num;
-    //grass type
-    types.grass_type.name = "grass";
-    types.grass_type.num = 2;   
-    types.grass_type.strengths=types.ground_type.num;
-    types.grass_type.weaknesses=types.fire_type.num;
-    //ground type
-    types.ground_type.name = "ground";
-    types.ground_type.num = 3;
-    types.ground_type.strengths=types.electric_type.num;
-    types.ground_type.weaknesses=types.grass_type.num;
-    //electric type
-    types.electric_type.name = "electric";
-    types.electric_type.num = 4;
-    types.electric_type.strengths=types.water_type.num;
-    types.electric_type.weaknesses=types.ground_type.num;
-
-    return 0;
-    
-};
-
-struct Attack{
-    string name;
-    int power;
-    Type type;
-};
-
-
-struct Copyrightmon{
+struct fakemon {
     string name;
     int max_hp;
-    int current_hp;
-    Type type;
+    int hp;
+    string type;
     int level;
-    Attack attacks[3];
+    vector<string> attacks;
 };
-
-Copyrightmon pikachu{
-    "Pikachu",
-    100,
-    100,
-    types.electric_type,
-    5,
-    {{"Exposed wire", 15, types.electric_type},
-    {"Static electricity", 20, types.electric_type},
-    {"Bzzt", 25, types.electric_type}}
-};
-
-int calculate_damage(Copyrightmon& attacker, Copyrightmon& defender, int attack_choice){
-    int base_damage = 10; // Base damage for all attacks
-    // Type effectiveness
-    base_damage += attacker.level * 2; // Increase damage based on level
-    if(attacker.type.strengths == defender.type.num){
-        base_damage *= 2; // Double damage for strengths
-    } else if(attacker.type.weaknesses == defender.type.num){
-        base_damage /= 2; // Half damage for weaknesses
-    }
-    return base_damage;
+int choose_fakemon(){
+    cout << "Choose your Fakemon:\n1. Flamester (Fire)\n2. Aquatail (Water)\n3. Leafy (Grass)\n4. Rocko (Ground)\n5. Sparky (Electric)" << endl;
+    int choice = input();
+    return choice;
 }
-
-int battle(Copyrightmon& player, Copyrightmon& opponent){
-    cout << "A wild " << opponent.name << " appeared!" << endl;
-    while(player.current_hp > 0 && opponent.current_hp > 0){
-        cout << "Choose an attack:" << endl;
-        cout << "1. " << player.attacks[0].name << endl;
-        cout << "2. "<< player.attacks[1].name << endl;
-        cout << "3. "<< player.attacks[2].name << endl;
+void attack_choice(fakemon attack_options){
+    for (int x; x < 4; x++){
+        cout<<x<<". "<<attack_options[x]<<endl;
+    }
+}
+int calculate_damage(fakemon& attacker, fakemon& defender, int attack_choice){
+    return 10;
+}
+bool battle(fakemon attacker, fakemon defender){
+    while (attacker.hp > 0 and defender.hp > 0){
+        
         int attack_choice = input();
-        int damage = calculate_damage(player, opponent, attack_choice);
-        opponent.current_hp -= damage;
-        cout << player.name << " used attack " << attack_choice << " and dealt " << damage << " damage!" << endl;
-        if(opponent.current_hp <= 0){
-            cout << opponent.name << " fainted!" << endl;
-            break;
+        int damage = calculate_damage(attacker, defender, attack_choice);
+        defender.hp -= damage;
+        cout << attacker.name << " used attack " << attack_choice << " and dealt " << damage << " damage!" << endl;
+        if(defender.hp <= 0){
+            cout << defender.name << " fainted!" << endl;
+            return true;
         }
-        int opp_damage = calculate_damage(opponent, player, 1);
-        player.current_hp -= opp_damage; // Opponent uses first attack
-        cout << opponent.name << " attacked and dealt " << opp_damage << " damage!" << endl;
-        if(player.current_hp <= 0){
-            cout << player.name << " fainted!" << endl;
-            break;
+        int opp_damage = calculate_damage(defender, attacker, 1);
+        attacker.hp -= opp_damage; // Opponent uses first attack
+        cout << defender.name << " attacked and dealt " << opp_damage << " damage!" << endl;
+        if(attacker.hp <= 0){
+            cout << attacker.name << " fainted!" << endl;
+            return false;
         }
     }
-    return 0;
+}
+void explore(fakemon player, fakemon encounter){
+    int encounter2 = random_number(4);
+    cout << "You explore the area and find a wild Fakemon!" << endl;
+    cout << "you have encountered a Wild" << encounter2 << endl;
+    int attacker = choose_fakemon();
+    bool outcome = battle(player, encounter);
+    if (outcome == true){
+        cout << "You have defeated the wild Fakemon!" << endl;
+        //add fakemon to player's collection
+    }else{
+        cout << "You failed!" << endl;
+    }
+
+
 }
 
 int main(){
-    type_setup();
     return 0;
-}
+ }
