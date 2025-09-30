11ab274537f68d78964600bc06df603d5c2638e7
cinnamonflavoredbismuth <cecily.strong@ucas-edu.net>
2025-09-30 11:48:18 -0600

    finished notes

--- Notes/text files/main.cpp
+++ Notes/text files/main.cpp
@@ -28,19 +28,25 @@ What do stream manipulators let us do?
 How do you write to a CSV?
     basically the same
 How do you read a text file?
-
+    file >> variable
 How do you read a CSV file?
-
+    basically the same
 What is a delimiter?
-
+    a seperator of values, such as a comma
 How do you read an entire CSV?
-
-How do you turn items from a CSV into objects of a structure?*/
+for line in csv:
+    split line by delimiter
+    store values in object
+How do you turn items from a CSV into objects of a structure?
+    create a structure that matches the data
+    read each line, split by delimiter, store in structure
+*/
 
 #include <iostream>
 #include <limits>
 #include <fstream>
 #include <iomanip>
+#include <vector>
 
 using namespace std;
 
@@ -60,13 +66,19 @@ int get_number(const string& prompt)   {
     return num;
 }
 
+struct Movie {
+    int id;
+    string title;
+    int year;
+};
 int main(){
     int first = 1;
     int second = 1;
     /*first = get_number("first: ");
     second = get_number("second: ");*/
-    cout << "you enetered " << first << " and " << second;
-
+    cout << "you enetered " << first << " and " << second << endl;
+     
+    //WRITE
     ofstream file;
     file.open("data.txt"); //create or open file
     if (file.is_open()){
@@ -81,5 +93,31 @@ int main(){
         file.close();
     }
 
+    //READ
+    ifstream ifile;
+    ifile.open("data.csv");
+    string str;
+    vector<Movie> movies;
+    if (ifile.is_open()){
+        while(!ifile.eof()){
+            getline(ifile, str,',');
+                if (str.empty()) continue;;
+            Movie movie;
+            movie.id = stoi(str);
+
+            getline(ifile, str,',');
+            movie.title = str;
+
+            getline(ifile, str,',');
+            movie.year = stoi(str);
+
+            movies.push_back(movie);
+        }
+        file.close();
+    }
+    for (Movie i:movies){
+            cout << "Title: " << i.title << "Year: " << i.year << endl;
+        }
+        
     return 0;
 }
--- high score lederboard/main.cpp
+++ high score lederboard/main.cpp
@@ -4,3 +4,18 @@ The program should allow the user to add new high scores, view the existing lead
 It should also read data from the file when the program starts. 
 The user menu should include options to add a new score, display the high scores, and exit. 
 The program must handle invalid inputs gracefully and continue running until the user chooses to exit.*/
+#include <iostream>
+#include <limits>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+void write(string file){
+    file.open(file); //create or open file
+    if (file.is_open()){
+}
+}
+int main(){
+    return 0;
+}
