8e971fcd65c56ea393c3952e23fa58cc62e53a40
cinnamonflavoredbismuth <cecily.strong@ucas-edu.net>
2025-10-13 11:47:37 -0600

    parent and child classes

---
+++ Notes/parent and child classes/main.py
@@ -0,0 +1,21 @@
+#CS Parent and Child Classes
+"""
+What is a parent/abstract class?
+    a broader class that includes other classes inside it
+How do you create a child class?
+    you make another class and connect it to the parent class
+How does a child class inherit from the parent class?
+    it has all the same methods
+What are class diagrams?
+    when you have a diagram of a class with methods determining their information (public, private, type, ect)
+How are class diagrams used to show a parent/child relationship?
+    a child has a white arrow pointing to a parent
+How do you overload operators in a class?
+
+What are test cases?
+    testing the minimum and maximum limits of the function to see if they work normally
+Why do we use test cases?
+    so you can see the limits of the program
+How do we create test cases?
+
+"""
